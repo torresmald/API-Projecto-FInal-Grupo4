e65ecc1d117a1b198c60fcb6051589f1c28da2b8
Jonathan Torres <jonathan.torresmald@gmail.com>
2023-03-29 20:49:48 +0200

    fixed cors

--- index.js
+++ index.js
@@ -20,24 +20,10 @@ const cors = require('cors');
 const http = require('http').Server(server);
 const io = require('socket.io')(http, {
   cors: {
-    origin: "http://localhost:4200",
-    allowedHeaders: ["my-custom-header"],
-    credentials: true
-  }
+    origin: ["http://localhost:4200"],
+}
 });
 
-// const whitelist = ['http://localhost:3000/', 'http://localhost:4200/', 'https://projecto-final-grupo4.vercel.app/' /** other domains if any */ ]
-// const corsOptions = {
-//   credentials: true,
-//   origin: function(origin, callback) {
-//     console.log(origin);
-//     if (whitelist.indexOf(origin) !== -1) {
-//       callback(null, true)
-//     } else {
-//       callback(new Error('Not allowed by CORS'))
-//     }
-//   }
-// };
 server.use(cors());
 
 connect();
